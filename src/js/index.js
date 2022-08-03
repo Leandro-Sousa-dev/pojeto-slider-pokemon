500b4c52dfd5334ddad2a7a39583f4eeaf8f4bcf
leandrosousa69 <95255973+leandrosousa69@users.noreply.github.com>
2022-08-03 15:28:13 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add('selecionado');
+}
+
+
+btnAvancar.addEventListener('click', function () {
+
+    esconderCartaoSelecionado();
+
+    if (cartaoAtual === cartoes.length - 1) {
+
+        cartaoAtual = 0;
+        
+        mostrarCartao(cartaoAtual);
+
+    }
+    else {
+        cartaoAtual++;
+        mostrarCartao(cartaoAtual);
+        
+    }
+
+});
+
+btnVoltar.addEventListener('click', function () {
+
+    esconderCartaoSelecionado();
+
+    if (cartaoAtual === 0) {
+
+        cartaoAtual = 2;
+
+        mostrarCartao(cartaoAtual);
+
+    }
+    else {
+        cartaoAtual--;
+
+        mostrarCartao(cartaoAtual);
+    }
+});
